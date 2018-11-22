0f55dd9aa573aff99a6245e73c1dee5dfc7a806f
Hamed <hamed.ehtesham.university@gmail.com>
2018-11-22 12:30:28 +0330

    fix: Invalid default value for prop "options": Props with type Object/Array must use a factory function to return the default value.

--- src/OverlayScrollbars.js
+++ src/OverlayScrollbars.js
@@ -12,7 +12,9 @@ export default {
     props: {
         options: {
             type: Object,
-            default: {}
+            default() {
+                return {};
+            }
         },
     },
 
